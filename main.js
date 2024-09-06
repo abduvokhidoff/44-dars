2600fb30c3f2e43bdcb5063dae3f9385397c1d3c
abduvokhidoff <abcdefgmananam@gmail.com>
2024-09-06 19:00:37 +0500

    done

---
+++ main.js
@@ -0,0 +1,50 @@
+// // let meningismim = prompt("Ismingizni Kiriting");
+// // console.log("ism", meningismim)
+
+
+
+// // let menignfamiliyam = prompt("Familiyangizni Kiriting");
+// // console.log("familiya", meningfamiliyam)
+
+
+// // let prompt1 = prompt('son 1 kiriting')
+// // let prompt2 = prompt('son 1 kiriting')
+// // let prompt3 = prompt('son 1 kiriting')
+
+
+// // let otvet = Math.max(prompt1,prompt2,prompt3)
+
+
+// // alert(`siz kirtgan eng katta son ${otvet}`)
+
+
+
+
+// // let pr = prompt('son kiritin')
+
+// // alert(Math.round(Math.random() * pr))  
+
+
+
+
+
+// let pr = prompt('1000')
+
+
+// alert =(Math.random()*pr)
+let ismingiz = prompt('Ismingizni kiriting')
+console.log("Ism:", ismingiz)
+
+let familiyangiz = prompt('Familiyiyangizni Kiriting')
+console.log("Familiya:", familiyangiz)
+
+let yoshingiz = prompt('Yoshingizni Kiriting')
+console.log("Yosh:", yoshingiz)
+
+let faningiz = prompt('Yaxshi korgan faningiz')
+console.log("Yaxshi korgan fan:", faningiz)
+
+let tuman = prompt('Yashaydigan Tumaningiz')
+console.log("Tuman:", tuman )
+
+alert('malumotlaringizni kiritganingiz uchun rahmat')
